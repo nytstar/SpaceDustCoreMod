1d2c292faadd12323c92cba44a08d391aeea3ce3
Unknown <csbiltz08@gmail.com>
2018-04-07 22:56:34 -0400

    Added Event Handlers

--- src/main/java/com/nytstalker/spacedustcoremod/Main.java
+++ src/main/java/com/nytstalker/spacedustcoremod/Main.java
@@ -4,8 +4,12 @@ import com.nytstalker.spacedustcoremod.proxy.CommonProxy;
 import com.nytstalker.spacedustcoremod.util.Reference;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
 public class Main {
@@ -16,7 +20,20 @@ public class Main {
 	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.COMMON_PROXY_CLASS)
 	public static CommonProxy proxy;
 	
+	@EventHandler
+	public static void PreInit(FMLPreInitializationEvent event) {
+		
+	}
 	
+	@EventHandler
+	public static void init(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public static void Postinit(FMLPostInitializationEvent event) {
+		
+	}
 	
 
 }
