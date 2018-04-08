5b914617070e11f2c8971f9ec267654e5d6d65af
Unknown <csbiltz08@gmail.com>
2018-04-07 22:52:26 -0400

    Added Proxy Classes

---
+++ src/main/java/com/nytstalker/spacedustcoremod/proxy/CommonProxy.java
@@ -0,0 +1,5 @@
+package com.nytstalker.spacedustcoremod.proxy;
+
+public class CommonProxy {
+
+}
--- src/main/java/com/nytstalker/spacedustcoremod/util/Reference.java
+++ src/main/java/com/nytstalker/spacedustcoremod/util/Reference.java
@@ -6,8 +6,8 @@ public class Reference {
 	public static final String NAME = "Space Dust Core Mod";
 	public static final String VERSION = "0.0";
 	public static final String ACCEPTED_VERSION = "[1.12.2]";
-	public static final String CLIENT_PROXY_CLASS = "";
-	public static final String COMMON_PROXY_CLASS = "";
+	public static final String CLIENT_PROXY_CLASS = "com.nytstalker.spacedustcoremod.proxy.ClientProxy";
+	public static final String COMMON_PROXY_CLASS = "com.nytstalker.spacedustcoremod.proxy.CommonProxy";
 	
 	
 	}
