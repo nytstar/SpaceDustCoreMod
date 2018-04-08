6e0956ffee805f8a5c2bcd14a58abdb352725f35
Unknown <csbiltz08@gmail.com>
2018-04-07 22:44:41 -0400

    This is my first commit
    Setup of the main class checking that gitkraken is working successfully.

---
+++ src/main/java/com/nytstalker/spacedustcoremod/Main.java
@@ -0,0 +1,10 @@
+package com.nytstalker.spacedustcoremod;
+
+import com.nytstalker.spacedustcoremod.util.Reference;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+public class Main {
+
+}
---
+++ src/main/java/com/nytstalker/spacedustcoremod/util/Reference.java
@@ -0,0 +1,13 @@
+package com.nytstalker.spacedustcoremod.util;
+
+public class Reference {
+
+	public static final String MOD_ID = "sdcm";
+	public static final String NAME = "Space Dust Core Mod";
+	public static final String VERSION = "0.0";
+	public static final String ACCEPTED_VERSION = "[1.12.2]";
+	public static final String CLIENT_PROXY_CLASS = "";
+	public static final String COMMON_PROXY_CLASS = "";
+	
+	
+	}
